4dbc79e8617559b86adf7bb66aa9c7f0739c4a83
Oleksandr Gavuka <oleksandr.gavuka@masterofcode.com>
2023-08-04 23:27:20 +0300

    updated /lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -9,10 +9,11 @@ export { PromptType } from './schema/PromptSchema';
 export { S3Service } from './S3Service';
 export { SessionStorage } from './session/SessionStorage';
 export { Session } from './session/Session';
-export { Config, SystemMessageComputer, OutputContext, InputPayload, SessionProps, ChatInputPayload, } from './@types/index';
+export { Config, SystemMessageComputer, OutputContext, InputPayload, SessionProps, ChatInputPayload, ErrorProperties, } from './@types/index';
 export { Message } from './session/Message';
 export { ChatHistory } from './session/ChatHistory';
 import { MiddlewareStatus as MdStatus } from './@types/index';
 export declare const MiddlewareStatuses: Record<MdStatus, MdStatus>;
 export type MiddlewareStatus = MdStatus;
+export { getContentOfChoiceByIndex, modifyContentOfChoiceByIndex, } from './helpers';
 //# sourceMappingURL=index.d.ts.map
