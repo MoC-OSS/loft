0b1f5f828abb39118cec3bdb5fa395565a739801
andrii-smahin-moc <andrii.smahin@masterofcode.com>
2023-09-13 12:02:21 +0300

    changed gitignore and remove lib

--- lib/FunctionManager.d.ts
+++
@@ -1,9 +0,0 @@
-import { OutputContext } from './@types';
-export type OpenAiFunctionArgs = any;
-export type OpenAiFunction = (ctx: OutputContext, args: OpenAiFunctionArgs) => Promise<string>;
-export declare class FunctionManager {
-    functions: Map<string, OpenAiFunction>;
-    constructor();
-    use(name: string, fn: OpenAiFunction): void;
-}
-//# sourceMappingURL=FunctionManager.d.ts.map
