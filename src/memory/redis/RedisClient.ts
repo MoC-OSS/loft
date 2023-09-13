a3c9220451839c891f8134c16d0f1cc9ef62e99f
andrii-smahin-moc <andrii.smahin@masterofcode.com>
2023-09-13 10:15:58 +0300

    added p-queue when set to redis

--- src/memory/redis/RedisClient.ts
+++ src/memory/redis/RedisClient.ts
@@ -1,5 +1,6 @@
 import Redis from 'ioredis';
 import Redlock from 'redlock';
+import PQueue from 'p-queue';
 
 import { getLogger } from '../../Logger';
 
@@ -7,22 +8,18 @@ const l = getLogger('redisService');
 
 export class RedisClient extends Redis {
   private redlock: Redlock;
-
+  private queue: PQueue;
   constructor(
     private readonly host: string,
     private readonly port: number,
     private readonly db: number,
   ) {
     super({ host, port, db });
-    this.redlock = new Redlock([this], {
-      driftFactor: 0.01,
-      retryCount: -1,
-      retryDelay: 100,
-      retryJitter: 100,
-    });
+    this.redlock = new Redlock([this]);
+    this.queue = new PQueue({ concurrency: 1 });
   }
 
-  async lockSet(
+  async setLock(
     key: string,
     value: string,
     expirationOptions?: {
@@ -30,17 +27,23 @@ export class RedisClient extends Redis {
       ttl: number;
     },
   ) {
-    const resource = `locks:${key}`;
-    const lock = await this.redlock.acquire([resource], 100);
-    expirationOptions
-      ? await this.set(
-          key,
-          value,
-          expirationOptions.secondsToken,
-          expirationOptions.ttl,
-        )
-      : await this.set(key, value);
-
-    await lock.release();
+    await this.queue.add(async () => {
+      const resource = `locks:${key}`;
+      const lock = await this.redlock.acquire([resource], 100);
+      try {
+        expirationOptions
+          ? await this.set(
+              key,
+              value,
+              expirationOptions.secondsToken,
+              expirationOptions.ttl,
+            )
+          : await this.set(key, value);
+      } catch (error) {
+        l.error(error);
+      } finally {
+        await lock.release();
+      }
+    });
   }
 }
